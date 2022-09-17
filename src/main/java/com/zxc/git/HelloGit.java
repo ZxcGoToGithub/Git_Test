d9a2da2e8665b22d4901dd5b3277ee3ba9f119b5
Zxc <zxcwjx77@163.com>
2022-09-17 20:38:55 +0800

    commit master版本1.1

--- src/main/java/com/zxc/git/HelloGit.java
+++ src/main/java/com/zxc/git/HelloGit.java
@@ -6,5 +6,6 @@ public class HelloGit {
         System.out.println("Git，我再说两句：你好，再见！");
         System.out.println("Git，直接提交到本地库，跳过添加至暂存区！git add");
         System.out.println("文件同时修改测试！！！");
+        System.out.println("测试冲突合并！！！master的修改！");
     }
 }
