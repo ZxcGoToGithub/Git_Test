e30d6e91f2c7cea067f8d2c5424dddb87465e2ef
Zxc <zxcwjx77@163.com>
2022-09-17 19:09:51 +0800

    commit 版本1.3

--- src/main/java/com/zxc/git/HelloGit.java
+++ src/main/java/com/zxc/git/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("你好，git，idea交给你了！");
         System.out.println("Git，我再说两句：你好，再见！");
+        System.out.println("Git，直接提交到本地库，跳过添加至暂存区！git add");
     }
 }
