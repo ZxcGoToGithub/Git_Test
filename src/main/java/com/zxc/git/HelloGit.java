72b6aadeffb926919ddcd6b0a1ce6fca0ff17386
Zxc <zxcwjx77@163.com>
2022-09-17 19:03:48 +0800

    commit 版本1.2

--- src/main/java/com/zxc/git/HelloGit.java
+++ src/main/java/com/zxc/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.zxc.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("你好，git，idea交给你了！");
+        System.out.println("Git，我再说两句：你好，再见！");
     }
 }
